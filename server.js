cc8de21e091beedb023b5cf28544ac5a7cd0f876
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-28 10:23:19 -0400

    modified schema and created seed

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ var express = require('express');
 var PORT = process.env.PORT || 8080;
 var app = express();
     app.use(express.static('public'));
-    app.use(express.urlenconded({ extended: true }));
+    app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
 // Setting up handlebars
 var exphbs = require('express-handlebars');
-    app.enging('handlebars', exphbs({ defaultLayout: 'main' }));
+    app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
     app.set('view engine', 'handlebars');
 
 // Importing routes and giving server access to the routes
-var routes = require('./controlers/burgerController.js');
-    app.use(routes);
+// var routes = require('./controllers/burgerController.js');
+//     app.use(routes);
 
 // Starting server listen
     app.listen(PORT, function(){
