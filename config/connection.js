160209c90fa7b0f652dd07508eb107a212f43ddc
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-29 00:56:07 -0400

    deploying app

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,16 @@
 var mysql = require('mysql');
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.MYSQL_KEY,
-    database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: process.env.MYSQL_KEY,
+        database: 'burger_db'
+    });
+};
 
 // Make the connection
 connection.connect(function(err) {
