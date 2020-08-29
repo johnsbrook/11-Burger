6b2cefc1fc93eaddb7a0f0f8de9043af918b76eb
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-28 23:57:29 -0400

    deploying app

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.MYSQL_KEY,
+    password: "Ivan0825",
     database: "burger_db"
 });
 
