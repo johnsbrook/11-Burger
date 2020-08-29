68b63341b43e9c377910dd51d39ade6e924e4f1a
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-29 00:18:03 -0400

    changed some instances in connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Ivan0825",
+    password: process.env.MYSQL_KEY,
     database: "burger_db"
 });
 
